732fb1ee6b2fb7b041804eddb5cf0134d905dca6
Grinil Khanna <gmkhanna@gmail.com>
2017-04-07 08:47:22 -0700

    Added compnents, model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FrontPageComponent } from './front-page/front-page.component';
+import { DetailComponent } from './detail/detail.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FrontPageComponent,
+    DetailComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
